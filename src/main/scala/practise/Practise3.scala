533705b96e56c78ae8e0c481128496c8f8533558
tammosminia <tammo.sminia@jdriven.com>
2015-03-10 12:11:28 +0100

    practise3

--- src/main/scala/practise/Practise3.scala
+++ src/main/scala/practise/Practise3.scala
@@ -1,5 +1,18 @@
 package practise
 
 object Practise3 {
-  def map(list: List[Int], method: (Int => Int)): List[Int] = ???
+  def addToAll(list: List[Int], amount: Int): List[Int] = ???
+
+  def findNegativeNumbers(list: List[Int]): List[Int] = ???
+
+  def firstPositiveNumber(list: List[Int]): Option[Int] = ???
+
+  def sum(list: List[Int]): Int = ???
+
+  //has a list of lists as input, sums up all elements
+  def sumAll(list: List[List[Int]]): Int = ???
+
+  //returns a list with all elements twice
+  def twiceList(list: List[Int]): List[Int] = ???
+
 }
--- src/test/scala/practise/Practise3Test.scala
+++ src/test/scala/practise/Practise3Test.scala
@@ -1,15 +1,38 @@
 package practise
 
 import org.scalatest.FunSuite
-import practise.Practise2._
+import practise.Practise3._
 
 class Practise3Test extends FunSuite {
 
-  test("map") {
-    assert(map(List(1, 2, 3), { _ + 1}) === List(2, 3, 4))
-    assert(map(List(2, 3, 4), { _ + 1}) === List(3, 4, 5))
-    assert(map(List(1, 2, 3), { _ * 2}) === List(2, 4, 6))
-    assert(map(List(1, 2, 3), { _ - 1}) === List(0, 1, 2))
+  test("addToAll") {
+    assert(addToAll(List(1, 2, 3), 1) === List(2, 3, 4))
+    assert(addToAll(List(1, 2, 3), 3) === List(4, 5, 6))
+  }
+
+  test("findNegativeNumbers") {
+    assert(findNegativeNumbers(List(-1, 1, -5)) === List(-1, -5))
+    assert(findNegativeNumbers(List(1, -5, -6)) === List(-5, -6))
+  }
+
+  test("firstPositiveNumber") {
+    assert(firstPositiveNumber(List(1, 2, 3)) === Some(1)) //Notice that it wraps the result in an option
+    assert(firstPositiveNumber(List(-1, -2)) === None)     //Because it may be none
+    assert(firstPositiveNumber(List(-1, -2, 4)) === Some(4))
+  }
+
+  test("sum") {
+    assert(sum(List(1, 2, 3)) === 6)
+    assert(sum(List(5, 5)) === 10)
+  }
+
+  test("sumAll") {
+    assert(sumAll(List(List(1, 2, 3), List(5, 5))) === 16)
+    assert(sumAll(List(List(1, 2, 3), List(1, 2, 3))) === 12)
+  }
+
+  test("twiceList") {
+    assert(twiceList(List(1, 2, 3)) === List(1, 1, 2, 2, 3, 3))
   }
 
 }
