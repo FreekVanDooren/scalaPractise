9f9fb80e2228ac7e17a18ce6907a426580a829a0
tammosminia <tammo.sminia@jdriven.com>
2015-03-13 09:42:22 +0100

    practise7 for comprehensions

---
+++ src/main/scala/practise/Practise7.scala
@@ -0,0 +1,12 @@
+package practise
+
+object Practise7 {
+  //create a list of all the combinations of letters and numbers (a1, a2, a3, b1, b2, .....)
+
+  //use map and flatmap here
+  def combinationsFlatMap(letters: List[String], numbers: List[Int]): List[String] = ???
+
+  //use for here
+  def combinationsFor(letters: List[String], numbers: List[Int]): List[String] = ???
+
+}
---
+++ src/test/scala/practise/Practise7Test.scala
@@ -0,0 +1,20 @@
+package practise
+
+import org.scalatest.FunSuite
+import practise.Practise7._
+
+class Practise7Test extends FunSuite {
+
+  test("combinationsFlatMap") {
+    testFunction(combinationsFlatMap)
+  }
+
+  test("combinationsFor") {
+    testFunction(combinationsFor)
+  }
+
+  def testFunction(function: (List[String], List[Int]) => List[String]) = {
+    assert(function(List("a", "b", "c"), List(1, 2, 3)) === List("a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"))
+  }
+
+}
