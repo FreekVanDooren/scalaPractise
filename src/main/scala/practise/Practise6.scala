8d7b809bf97946c330959fdfa662a194e082dd14
tammosminia <tammosminia@gmail.com>
2015-03-13 16:33:22 +0100

    no easy answers

--- src/main/scala/practise/Practise6.scala
+++ src/main/scala/practise/Practise6.scala
@@ -4,11 +4,11 @@ object Practise6 {
   //hint: flatmap
 
   //make one list with all words in the sentences
-  def allWordsIn(sentences: List[String]): List[String] = sentences.flatMap { _.split(" ") }
+  def allWordsIn(sentences: List[String]): List[String] = ???
 
   //hint: List.fill
-  def repeatWord(times: Int, word: String): List[String] = List.fill(times)(word)
-  def repeatWords(input: List[(Int, String)]): List[String] = input.flatMap { case (times, word) => repeatWord(times, word) }
+  def repeatWord(times: Int, word: String): List[String] = ???
+  def repeatWords(input: List[(Int, String)]): List[String] = ???
 
 
 }
