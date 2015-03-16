920545912eb84d579235b155ed53cbc3845c3c96
tammosminia <tammosminia@gmail.com>
2015-03-16 11:49:53 +0100

    introduced a helper function

--- src/main/scala/practise/Practise7.scala
+++ src/main/scala/practise/Practise7.scala
@@ -3,10 +3,11 @@ package practise
 object Practise7 {
   //create a list of all the combinations of letters and numbers (a1, a2, a3, b1, b2, .....)
 
-  //use map and flatmap here
+  //a helper function for combinationsFlatMap
+  def combinationsForOneLetter(letter: String, numbers: List[Int]): List[String] = ???
   def combinationsFlatMap(letters: List[String], numbers: List[Int]): List[String] = ???
 
-  //use for here
+  //use for and yield here
   def combinationsFor(letters: List[String], numbers: List[Int]): List[String] = ???
 
 }
