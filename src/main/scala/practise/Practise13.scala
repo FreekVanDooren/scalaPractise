6d99182dfa12e84955e39230a7070ba32f7d8352
Tammo Sminia <tammo.sminia@jdriven.com>
2015-03-27 09:53:08 +0100

    recursion

---
+++ src/main/scala/practise/Practise13.scala
@@ -0,0 +1,9 @@
+package practise
+
+object Practise13 {
+  //Given a non-negative int n, return the sum of its digits.
+  //hint: modulo (%) and recursion
+
+  def sumDigits(x: Int): Int = ???
+
+}
---
+++ src/test/scala/practise/Practise13Test.scala
@@ -0,0 +1,26 @@
+package practise
+
+import org.scalatest.FunSuite
+import practise.Practise13._
+
+class Practise13Test extends FunSuite {
+  test("sumDigits - 0") {
+    assert(sumDigits(0) === 0)
+  }
+
+  test("sumDigits - 1") {
+    assert(sumDigits(1) === 1)
+  }
+
+  test("sumDigits - 11") {
+    assert(sumDigits(11) === 2)
+  }
+
+  test("sumDigits - 123") {
+    assert(sumDigits(123) === 6)
+  }
+
+  test("sumDigits - 99") {
+    assert(sumDigits(99) === 18)
+  }
+}
