68f8ef72d01950fa9c4aa09230bfa80cb213cf88
Tammo Sminia <tammo.sminia@jdriven.com>
2015-04-08 15:10:49 +0200

    lists

--- src/main/scala/practise/Practise19.scala
+++ src/main/scala/practise/Practise19.scala
@@ -3,19 +3,15 @@ package practise
 object Practise19 {
   //read: http://www.tutorialspoint.com/scala/scala_lists.htm
 
-  def emptyList = Nil
+  def emptyList = ???
 
   //create a list with List(1, 2, 3
-  def twoThree = List(2, 3)
+  def twoThree = ???
 
   //add a one at the start of the list using ::
-  def oneTwoThree = 1 :: twoThree
+  def oneTwoThree = ???
 
   //You can also use :: for pattern matching
-  def matchList(list: List[Any]): String = list match {
-    case Nil => "empty"
-    case (x: Int) :: _ => "starts with a number"
-    case x :: _ => "starts with " + x
-  }
+  def matchList(list: List[Any]): String = ???
   
 }
