9fcd4d6164b26cf54f7983e3dc746a213e5b94ef
tammosminia <tammosminia@gmail.com>
2015-08-21 16:40:04 +0200

    include answers

--- src/test/scala/practise/chapter3patternmatching/Exercise2List.scala
+++ src/test/scala/practise/chapter3patternmatching/Exercise2List.scala
@@ -7,13 +7,23 @@ class Exercise2List extends FunSuite {
   }
   //startAnswer
   object Answer {
-    //hint: match with ::
+    //try to match with ::
+    def firstElement(list: List[Int]) = list match {
+      case first :: _ => first
+    }
+
     def first2Elements(list: List[Int]) = list match {
       case first :: second :: _ => List(first, second)
     }
   }
   import Answer._
   //endAnswer
+
+  test("firstElement") {
+    assert(first2Elements(1 :: Nil) === 1)
+    assert(first2Elements(1 :: 2 :: 3 :: Nil) === 1)
+    assert(first2Elements(List(5, 6, 7, 8)) === 5)
+  }
 
   test("first2Elements") {
     assert(first2Elements(1 :: 2 :: Nil) === List(1, 2))
--- src/test/scala/practise/chapter3patternmatching/Exercise3CaseClasses.scala
+++ src/test/scala/practise/chapter3patternmatching/Exercise3CaseClasses.scala
@@ -2,16 +2,28 @@ package practise.chapter3patternmatching
 
 import org.scalatest.FunSuite
 
-object Practise5 {
+class Exercise3CaseClasses extends FunSuite {
   case class Singer(name: String)
   case class Programmer(firstName: String)
 
-  //hint: use match/case
-  def nameOf(o: Object): String = ???
-}
+  object Exercise {
+  }
+  //startAnswer
+  object Answer {
+    def toppers: List[Singer] = List(Singer("René Froger"), Singer("Gordon"), Singer("Gerard Joling"))
 
-class Exercise3CaseClasses extends FunSuite {
-  import Practise5._
+    //hint: use match/case
+    def nameOf(o: Object): String = o match {
+      case Singer(name) => name
+      case Programmer(name) => name
+    }
+  }
+  import Answer._
+  //endAnswer
+
+  test("toppers") {
+    assert(toppers.map(_.name).sameElements(List("Gordon", "René Froger", "Gerard Joling")))
+  }
 
   test("match") {
     assert(nameOf(Singer("Dries Roelvink")) === "Dries Roelvink")
